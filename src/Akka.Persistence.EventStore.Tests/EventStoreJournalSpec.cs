dd8bc60398d957da105f1173665a874dedf67d05
Christopher Martin <chris.martin@eventday.com>
2015-04-15 21:02:08 -0700

    first commit

---
+++ src/Akka.Persistence.EventStore.Tests/EventStoreJournalSpec.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using Akka.Configuration;
+using Akka.Persistence.TestKit.Journal;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Embedded;
+using EventStore.Core;
+
+namespace Akka.Persistence.EventStore.Tests
+{
+    public class EventStoreJournalSpec : JournalSpec
+    {
+        private static readonly Config SpecConfig = ConfigurationFactory.ParseString(@"
+        akka.persistence {
+            publish-plugin-commands = on
+            journal {
+                plugin = ""akka.persistence.journal.eventstore""
+                eventstore {
+                    class = ""Akka.Persistence.EventStore.Journal.EventStoreJournal, Akka.Persistence.EventStore""
+                    plugin-dispatcher = ""akka.actor.default-dispatcher""
+                    server-host = ""127.0.0.1""
+                    server-tcp-port = 4532
+                    connection-settings-factory = ""Akka.Persistence.EventStore.DefaultConnectionSettingsFactory, Akka.Persistence.EventStore""
+                }
+            }
+        }");
+
+//        private static readonly ClusterVNode Node;
+
+        //This shouldn't be necessary. The base ctor starts sending messages before I can spin up my ES node. BOO!
+        static EventStoreJournalSpec()
+        {
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4532);
+            //BUG: Not working anyway. use your running server.
+//            Node = EmbeddedVNodeBuilder
+//                .AsSingleNode()
+//                .WithInternalTcpOn(tcpEndPoint)
+//                .WithInternalHttpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5553))
+//                .Build();
+//
+//            Node.Start();
+        }
+
+        public EventStoreJournalSpec()
+            : base(SpecConfig, "EventStoreJournalSpec")
+        {}
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+//            Node.Stop();
+        }
+    }
+}
---
+++ src/Akka.Persistence.EventStore/Extension.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Akka.Persistence.EventStore
+{
+    /// <summary>
+    /// Configuration settings representation targeting Sql Server journal actor.
+    /// </summary>
+    public class JournalSettings
+    {
+        public const string ConfigPath = "akka.persistence.journal.eventstore";
+
+        /// <summary>
+        /// Connection string used to access a persistent EventStore instance.
+        /// </summary>
+        public string Host { get; private set; }
+
+        /// <summary>
+        /// Connection timeout for EventStore related operations.
+        /// </summary>
+        public int TcpPort { get; private set; }
+
+        /// <summary>
+        /// Connection settings for EventStore connections.
+        /// </summary>
+        public IConnectionSettingsFactory ConnectionSettingsFactory { get; private set; }
+
+        public JournalSettings(Config config)
+        {
+            if (config == null) throw new ArgumentNullException("config", "EventStore journal settings cannot be initialized, because required HOCON section couldn't been found");
+
+            Host = config.GetString("server-host");
+            TcpPort = config.GetInt("server-tcp-port");
+
+            var settingsFactoryType = Type.GetType(config.GetString("connection-settings-factory"));
+            ConnectionSettingsFactory = settingsFactoryType != null
+                ? (IConnectionSettingsFactory) Activator.CreateInstance(settingsFactoryType)
+                : new DefaultConnectionSettingsFactory();
+        }
+    }
+
+    /// <summary>
+    /// Configuration settings representation targeting Sql Server snapshot store actor.
+    /// </summary>
+    public class SnapshotStoreSettings
+    {
+        public const string ConfigPath = "akka.persistence.snapshot-store.eventstore";
+
+        /// <summary>
+        /// Connection string used to access a persistent EventStore instance.
+        /// </summary>
+        public string Host { get; private set; }
+
+        /// <summary>
+        /// Connection timeout for EventStore related operations.
+        /// </summary>
+        public int TcpPort { get; private set; }
+
+        /// <summary>
+        /// Connection settings for EventStore connections.
+        /// </summary>
+        public IConnectionSettingsFactory ConnectionSettingsFactory { get; private set; }
+
+        public SnapshotStoreSettings(Config config)
+        {
+            if (config == null) throw new ArgumentNullException("config", "EventStore journal settings cannot be initialized, because required HOCON section couldn't been found");
+
+            Host = config.GetString("server-host");
+            TcpPort = config.GetInt("server-tcp-port");
+
+            var settingsFactoryType = Type.GetType(config.GetString("connection-settings-factory"));
+            ConnectionSettingsFactory = settingsFactoryType != null
+                ? (IConnectionSettingsFactory)Activator.CreateInstance(settingsFactoryType)
+                : new DefaultConnectionSettingsFactory();
+        }
+    }
+
+    /// <summary>
+    /// An actor system extension initializing support for EventStore persistence layer.
+    /// </summary>
+    public class EventStorePersistenceExtension : IExtension
+    {
+        /// <summary>
+        /// Journal-related settings loaded from HOCON configuration.
+        /// </summary>
+        public readonly JournalSettings JournalSettings;
+
+        /// <summary>
+        /// Snapshot store related settings loaded from HOCON configuration.
+        /// </summary>
+        public readonly SnapshotStoreSettings SnapshotStoreSettings;
+
+        public EventStorePersistenceExtension(ExtendedActorSystem system)
+        {
+            system.Settings.InjectTopLevelFallback(EventStorePersistence.DefaultConfiguration());
+
+            JournalSettings = new JournalSettings(system.Settings.Config.GetConfig(JournalSettings.ConfigPath));
+            SnapshotStoreSettings = new SnapshotStoreSettings(system.Settings.Config.GetConfig(SnapshotStoreSettings.ConfigPath));
+        }
+    }
+
+    /// <summary>
+    /// Singleton class used to setup EventStore backend for akka persistence plugin.
+    /// </summary>
+    public class EventStorePersistence : ExtensionIdProvider<EventStorePersistenceExtension>
+    {
+        public static readonly EventStorePersistence Instance = new EventStorePersistence();
+
+        /// <summary>
+        /// Initializes a EventStore persistence plugin inside provided <paramref name="actorSystem"/>.
+        /// </summary>
+        public static void Init(ActorSystem actorSystem)
+        {
+            Instance.Apply(actorSystem);
+        }
+
+        private EventStorePersistence() { }
+
+        /// <summary>
+        /// Creates an actor system extension for akka persistence EventStore support.
+        /// </summary>
+        /// <param name="system"></param>
+        /// <returns></returns>
+        public override EventStorePersistenceExtension CreateExtension(ExtendedActorSystem system)
+        {
+            return new EventStorePersistenceExtension(system);
+        }
+
+        /// <summary>
+        /// Returns a default configuration for akka persistence EventStore-based journals and snapshot stores.
+        /// </summary>
+        /// <returns></returns>
+        public static Config DefaultConfiguration()
+        {
+            return ConfigurationFactory.FromResource<EventStorePersistence>("Akka.Persistence.EventStore.eventstore.conf");
+        }
+    }
+}
